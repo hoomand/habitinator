9e34dabee50a9b2bd1d3d65df16253c035fe860b
Bijan Hoomand <hoomand@gmail.com>
2018-01-19 17:52:57 +1100

    add users, categories and goals controllers

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,16 @@
+class CategoriesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  get 'goals/index'
+
+  get 'goals/show'
+
+  get 'goals/edit'
+
+  get 'goals/update'
+
+  get 'goals/delete'
+
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'categories/edit'
+
+  get 'categories/update'
+
+  get 'categories/delete'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+  get 'users/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
