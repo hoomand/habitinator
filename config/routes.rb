b279cb16e962404f0351d001a3a89f2715dc982b
Bijan Hoomand <hoomand@gmail.com>
2018-01-29 19:26:38 +1100

    add root route for after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :users
-  root "home#index"
 
   namespace :admin do
     resources :goals
     resources :categories
     resources :users
+
   end
 
-  resources :goals
-  resources :categories
-  resources :users, only: [:show, :edit, :update]
+  authenticated :user do
+    root to: 'admin/goals#index', as: :authenticated_root
+  end
 
+  root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
