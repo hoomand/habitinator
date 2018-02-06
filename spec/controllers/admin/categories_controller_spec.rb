1baeb5c0a1563b971240ed9a9f7a09479cfefa85
Bijan Hoomand <hoomand@gmail.com>
2018-02-07 09:48:09 +1100

    add categories test

---
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Admin::CategoriesController, type: :controller do
+  describe "anonymous user" do
+    before :each do
+      # This simulates an anonymous user
+      login_with nil
+    end
+
+    it "should be redirected to signin" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe "logged in user" do
+    before :each do
+      login_with create(:user)
+    end
+    it "should see all his categoriess" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+end
