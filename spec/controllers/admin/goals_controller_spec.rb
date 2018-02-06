9a902b58291b18b6ebd025c5634f6e18f4803c2d
Bijan Hoomand <hoomand@gmail.com>
2018-02-07 09:47:54 +1100

    add a separate section for logged in user test

--- spec/controllers/admin/goals_controller_spec.rb
+++ spec/controllers/admin/goals_controller_spec.rb
@@ -9,13 +9,17 @@ RSpec.describe Admin::GoalsController, type: :controller do
 
     it "should be redirected to signin" do
       get :index
-      expect( response ).to redirect_to( new_user_session_path )
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
-  it "should let a user see all his goals" do
-    login_with create( :user )
-    get :index
-    expect( response ).to render_template( :index )
+  describe "logged in user" do
+    before :each do
+      login_with create(:user)
+    end
+    it "should see all his goals" do
+      get :index
+      expect(response).to render_template(:index)
+    end
   end
 end
