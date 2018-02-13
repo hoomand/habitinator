2f383d57637e558b9e7ee51f9df0a9cd4e0ef5e0
Bijan Hoomand <hoomand@gmail.com>
2018-02-14 09:59:51 +1100

    add human friendly created/updated dates

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,4 +9,12 @@ class Category < ApplicationRecord
 
   validates :name, presence: true
   validates :unit_type, presence: true
+
+  def created_at_formatted
+    created_at.to_time.strftime('%B %e at %l:%M %p')
+  end
+
+  def updated_at_formatted
+    created_at.to_time.strftime('%B %e at %l:%M %p')
+  end
 end
