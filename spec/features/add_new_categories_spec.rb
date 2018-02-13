ddfc06bacf96271fb0791e277dc7f1c8e55cc06d
Bijan Hoomand <hoomand@gmail.com>
2018-02-14 09:08:35 +1100

    refactor: use Warden login_as & rspec before_each
    Replace our sign_in_as with Warden login_as
    Use before_each to do the sign in and instantiate category

--- spec/features/add_new_categories_spec.rb
+++ spec/features/add_new_categories_spec.rb
@@ -1,37 +1,28 @@
 require 'rails_helper'
-require_relative '../helpers/authentication'
-
-RSpec.configure do |c|
-  c.include Helpers::Authentication
-end
 
 feature "Signed in user creates category", type: :feature do
 
+  before(:each) do
+    @user = create(:user)
+    login_as @user
+    @category_book = build(:category_book, user: @user)
+  end
+
   scenario 'with valid input' do
-    u = create(:user)
-    sign_in_as(u)
-
-    category_book = build(:category_book, user: u)
-
     visit new_admin_category_path
-    fill_in 'category[name]', with: category_book.name
+    fill_in 'category[name]', with: @category_book.name
     choose 'category_unit_type_number'
-    fill_in 'category[unit_name]', with: category_book.unit_name
+    fill_in 'category[unit_name]', with: @category_book.unit_name
     click_button 'Submit'
 
     expect(page).to have_content "Category created successfully"
   end
 
   scenario 'with missing input' do
-    u = create(:user)
-    sign_in_as(u)
-
-    category_book = build(:category_book, user: u)
-
     visit new_admin_category_path
     fill_in 'category[name]', with: ''
     choose 'category_unit_type_number'
-    fill_in 'category[unit_name]', with: category_book.unit_name
+    fill_in 'category[unit_name]', with: @category_book.unit_name
     click_button 'Submit'
 
     expect(page).to have_content "Name can't be blank"
