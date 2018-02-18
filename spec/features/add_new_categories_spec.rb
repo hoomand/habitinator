d7b30c5051dcfd845a2db197ba144da1d1c55e4a
Bijan Hoomand <hoomand@gmail.com>
2018-02-19 09:39:54 +1100

    add signed in & anynomous user contexts to category feature test

--- spec/features/add_new_categories_spec.rb
+++ spec/features/add_new_categories_spec.rb
@@ -1,30 +1,45 @@
 require 'rails_helper'
 
-feature "Signed in user creates category", type: :feature do
+feature "category management", type: :feature do
 
-  before(:each) do
-    @user = create(:user)
-    login_as @user
-    @category_book = build(:category_book, user: @user)
-  end
 
-  scenario 'with valid input' do
-    visit new_admin_category_path
-    fill_in 'category[name]', with: @category_book.name
-    choose 'category_unit_type_number'
-    fill_in 'category[unit_name]', with: @category_book.unit_name
-    click_button 'Submit'
+  context 'signed-in user' do
+    before(:each) do
+      @user = create(:user)
+      login_as @user
+    end
+    scenario 'creates category of type Numeric' do
+      category_book = build(:category_book, user: @user)
+      visit new_admin_category_path
+      fill_in 'category[name]', with: category_book.name
+      choose 'category_unit_type_number'
+      fill_in 'category[unit_name]', with: category_book.unit_name
+      click_button 'Submit'
+
+      expect(page).to have_content "Category created successfully"
+    end
+
+    scenario 'creates category without a name' do
+      category_book = build(:category_book, user: @user)
+      visit new_admin_category_path
+      fill_in 'category[name]', with: ''
+      choose 'category_unit_type_number'
+      fill_in 'category[unit_name]', with: category_book.unit_name
+      click_button 'Submit'
 
     expect(page).to have_content "Category created successfully"
   end
 
-  scenario 'with missing input' do
-    visit new_admin_category_path
-    fill_in 'category[name]', with: ''
-    choose 'category_unit_type_number'
-    fill_in 'category[unit_name]', with: @category_book.unit_name
-    click_button 'Submit'
+  context 'anonymous user gets denied' do
+    scenario 'listing categories' do
+      visit admin_categories_path
+      expect(page).to have_content "You need to sign in or sign up before continuing"
+    end
 
-    expect(page).to have_content "Name can't be blank"
+    scenario 'creating categories' do
+      visit new_admin_category_path
+      expect(page).to have_content "You need to sign in or sign up before continuing"
+    end
   end
+
 end
