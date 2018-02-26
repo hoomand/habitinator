b5b4b50446ca5ecbb69105c3f8c4ecae0111e31b
Bijan Hoomand <hoomand@gmail.com>
2018-02-26 13:02:10 +1100

    fix missing font path in test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,4 +40,5 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.relative_url_root = '/'
+  config.assets.debug = true
 end
