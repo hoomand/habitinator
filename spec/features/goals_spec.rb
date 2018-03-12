bcb51190f140b6cb9c1cdccd8572bab32c0c9869
Bijan Hoomand <hoomand@gmail.com>
2018-03-13 09:01:47 +1100

    test goal value visibility for different category types

--- spec/features/goals_spec.rb
+++ spec/features/goals_spec.rb
@@ -9,12 +9,19 @@ feature "goal management", type: :feature do
       login_as @user
       @cb = create(:category_book, user: @user)
       @cg = create(:category_gym, user: @user)
+      @category_kindle = create(:category_kindle, user: @user)
     end
     scenario 'creates a book-reading goal' do
       read_book = build(:read_Ready_Player_One_book, category: @cb)
       visit new_admin_goal_path
       fill_in 'goal[name]', with: read_book.name
+      select @cb.name, from: "goal[category_id]"
       choose "goal_frequency_#{read_book.frequency}"
+
+      # When category is numeric, we should be able to enter a goal value
+      # For example for a book, we can enter 500 to show its number of pages
+      expect(page).to have_selector('#goal_value_div', visible: true)
+
       fill_in 'goal[goal_value]', with: read_book.goal_value
       click_button 'Submit'
       expect(page).to have_content "Goal created successfully"
@@ -24,7 +31,13 @@ feature "goal management", type: :feature do
       gym = build(:attend_bodypump, category: @cg)
       visit new_admin_goal_path
       fill_in 'goal[name]', with: gym.name
+      select @cg.name, from: "goal[category_id]"
       choose "goal_frequency_#{gym.frequency}"
+
+      # When category is not of type numeric, goal value should not be visible
+      # For gym, you just want to go, or for kindle book, goal value is 100% obviously
+      expect(page).to have_selector('#goal_value_div', visible: false)
+
       click_button 'Submit'
       expect(page).to have_content "Goal created successfully"
     end
@@ -33,9 +46,15 @@ feature "goal management", type: :feature do
       read_book = create(:read_Ready_Player_One_book, category: @cb)
       visit admin_goals_path
       click_link("edit_goal_#{read_book.id}")
-      fill_in 'goal[name]', with: "#{read_book.name}_editted"
-      select @cg.name, from: 'goal[category_id]'
+
+      # goal value should be visible for a category of type numeric, i.e. book
+      expect(page).to have_selector('#goal_value_div', visible: true)
+      fill_in 'goal[name]', with: "#{read_book.name}_kindle_version"
+      select @category_kindle.name, from: 'goal[category_id]'
+      # goal value should become invisible after changing category to kindle
+      expect(page).to have_selector('#goal_value_div', visible: true)
       choose "goal_frequency_monthly"
+
       click_button 'Submit'
       expect(page).to have_content "Goal updated"
     end
