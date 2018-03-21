a3512035bfdaae817471a69e7ffe4350a0059e52
Bijan Hoomand <hoomand@gmail.com>
2018-03-22 10:03:04 +1100

    return goal value for all categories & show unit name

--- spec/features/goals_spec.rb
+++ spec/features/goals_spec.rb
@@ -18,10 +18,6 @@ feature "goal management", type: :feature do
       select @category_book.name, from: "goal[category_id]"
       choose "goal_frequency_#{read_book.frequency}"
 
-      # When category is numeric, we should be able to enter a goal value
-      # For example for a book, we can enter 500 to show its number of pages
-      expect(page).to have_selector('#goal_value_div', visible: true)
-
       fill_in 'goal[goal_value]', with: read_book.goal_value
       click_button 'Submit'
       expect(page).to have_content "Goal created successfully"
@@ -33,10 +29,7 @@ feature "goal management", type: :feature do
       fill_in 'goal[name]', with: gym.name
       select @category_gym.name, from: "goal[category_id]"
       choose "goal_frequency_#{gym.frequency}"
-
-      # When category is not of type numeric, goal value should not be visible
-      # For gym, you just want to go, or for kindle book, goal value is 100% obviously
-      expect(page).to have_selector('#goal_value_div', visible: false)
+      fill_in 'goal[goal_value]', with: gym.goal_value
 
       click_button 'Submit'
       expect(page).to have_content "Goal created successfully"
@@ -47,11 +40,9 @@ feature "goal management", type: :feature do
       visit admin_goals_path
       click_link("edit_goal_#{read_book.id}")
 
-      # goal value should be visible for a category of type numeric, i.e. book
       expect(page).to have_selector('#goal_value_div', visible: true)
       fill_in 'goal[name]', with: "#{read_book.name}_kindle_version"
       select @category_kindle.name, from: 'goal[category_id]'
-      # goal value should become invisible after changing category to kindle
       expect(page).to have_selector('#goal_value_div', visible: true)
       choose "goal_frequency_monthly"
 
