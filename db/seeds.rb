7f443af424fb9716cc4134bcbd13f4dfb97d6ac7
Bijan Hoomand <hoomand@gmail.com>
2018-04-01 18:15:52 +1000

    seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_admin = User.create!(
+    email: 'root@example.com',
+    first_name: 'root',
+    last_name: 'root',
+    password: 'adminuser',
+    password_confirmation: 'adminuser',
+    role: :admin
+)
