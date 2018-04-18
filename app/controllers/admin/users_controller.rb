1e77e8ef2da675e7bd29da2f4923124121cfcc3d
Bijan Hoomand <hoomand@gmail.com>
2018-04-18 12:57:05 +1000

    add user edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,9 +8,21 @@ module Admin
 
     def show; end
 
-    def edit; end
+    def edit
+      @user = find_or_redirect(:user, params[:id])
+    end
 
-    def update; end
+    def update
+      user = find_or_redirect(:user, params[:id])
+      return if user.nil?
+      if user.update(user_params)
+        flash[:notice] = "User #{user.email} was successfully updated"
+        redirect_to admin_users_path
+      else
+        flash[:alert] = "Could not update #{user.email}"
+        redirect_to edit_admin_user_path(params[:id])
+      end
+    end
 
     def destroy
       user = find_or_redirect(:user, params[:id])
@@ -21,6 +33,14 @@ module Admin
         flash[:notice] = "User #{user.email} was successfully deleted"
       end
       redirect_to admin_users_path
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name,
+                                   :last_name,
+                                   :role)
     end
   end
 end
