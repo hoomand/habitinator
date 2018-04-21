8f93733d3617653a74bcf90d480c956b52caef97
Bijan Hoomand <hoomand@gmail.com>
2018-04-21 20:44:02 +1000

    refactor admin home page

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,5 +1,15 @@
 module Admin
   class HomeController < Admin::ApplicationController
-    def index; end
+    before_action :authenticate_user!
+    def index
+      @new_users_count = User.where("created_at >= ?", 1.month.ago.utc).count
+      @new_categories_count = Category.where("created_at >= ?", 1.month.ago.utc).count
+      @new_goals_count = Goal.where("created_at >= ?", 1.month.ago.utc).count
+      @new_ledgers_count = Ledger.where("created_at >= ?", 1.month.ago.utc).count
+
+      @total_categories = Category.count
+      @total_goals = Goal.count
+      @total_ledgers = Ledger.count
+    end
   end
 end
